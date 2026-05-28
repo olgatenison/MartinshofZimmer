2ca24e4ae8562843e829112a3f72e9ae5c668744
olgatenison <88651744+olgatenison@users.noreply.github.com>
2026-05-28 18:24:32 +0300

    for Anna

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -113,30 +113,3 @@ export default async function LocaleLayout({ children, params }: Props) {
     </html>
   );
 }
-// import { NextIntlClientProvider } from "next-intl";
-// import { setRequestLocale } from "next-intl/server";
-// import "../globals.css";
-
-// export default async function LocaleLayout({
-//   children,
-//   params,
-// }: {
-//   children: React.ReactNode;
-//   params: Promise<{ locale: string }>;
-// }) {
-//   const { locale } = await params;
-
-//   setRequestLocale(locale);
-
-//   const messages = (await import(`../../messages/${locale}.json`)).default;
-
-//   return (
-//     <html lang={locale}>
-//       <body>
-//       <NextIntlClientProvider locale={locale} messages={messages}>
-//         {children}
-//         </NextIntlClientProvider>
-//       </body>
-//     </html>
-//   );
-// }
